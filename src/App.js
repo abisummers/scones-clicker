66c57f607901b586a80d8c3c22d2bde02a203750
abisummers <33722696+abisummers@users.noreply.github.com>
2018-03-18 14:50:21 +0100

    Initial commit
    Made with create-react-app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = { numberOfScones: 0 };
+  handleClick = () => {
+    console.log("Hello");
+    this.setState(state => {
+      return { numberOfScones: state.numberOfScones + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1> Scone Clicker </h1>
+
+        <button onClick={this.handleClick}>Click Here!</button>
+
+        <h2> {this.state.numberOfScones} </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
